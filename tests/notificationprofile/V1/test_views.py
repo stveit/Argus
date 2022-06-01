a2d72198152a8803ba5f97e8b0582d662a40074c
Simon Oliver Tveit <simtve@uninett.no>
2022-06-01 10:01:03 +0200

    Test filterpreview
    remove #todo, i think all the different
    endpoints are tested now. Next to do is
    have more tests for each of the endpoints
    to test different edge cases
    
    Add dependencies that dissapeared
    
    Set filter1 as an attribute

--- tests/notificationprofile/V1/test_views.py
+++ tests/notificationprofile/V1/test_views.py
@@ -11,6 +11,13 @@ from argus.notificationprofile.factories import (
     FilterFactory,
     NotificationProfileFactory,
 )
+
+from argus.notificationprofile.models import (
+    Filter,
+    Timeslot,
+    NotificationProfile,
+)
+
 from argus.notificationprofile.models import NotificationProfile
 from argus.util.testing import disconnect_signals, connect_signals
 
@@ -28,13 +35,18 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
 
         self.timeslot1 = TimeslotFactory(user=self.user1, name="Never")
         self.timeslot2 = TimeslotFactory(user=self.user1, name="Never 2: Ever-expanding Void")
-        filter1 = FilterFactory(
+        self.filter1 = FilterFactory(
             user=self.user1,
             name="Critical incidents",
             filter_string=f'{{"sourceSystemIds": [{self.source1.pk}]}}',
         )
         self.notification_profile1 = NotificationProfileFactory(user=self.user1, timeslot=self.timeslot1)
-        self.notification_profile1.filters.add(filter1)
+        self.notification_profile1.filters.add(self.filter1)
+        self.filter2 = FilterFactory(
+            user=self.user1,
+            name="Unused filter",
+            filter_string=f'{{"sourceSystemIds": [{self.source1.pk}]}}',
+        )
         self.notification_profile1.destinations.set(self.user1.destinations.all())
         self.media = []
         self.phone_number = None
@@ -53,6 +65,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         connect_signals()
 
     def test_incidents_filtered_by_notification_profile_view(self):
+        # /api/v1/notificationprofiles/<int:pk>/incidents/
         response = self.user1_rest_client.get(
             reverse("v1:notification-profile:notificationprofile-incidents", args=[self.notification_profile1.pk])
         )
@@ -61,8 +74,8 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
 
     def test_notification_profile_can_properly_change_timeslot(self):
         profile1_pk = self.notification_profile1.pk
+        # /api/v1/notificationprofiles/<int:pk>/
         profile1_path = reverse("v1:notification-profile:notificationprofile-detail", args=[profile1_pk])
-
         self.assertEqual(self.user1.notification_profiles.get(pk=profile1_pk).timeslot, self.timeslot1)
         self.assertEqual(self.user1_rest_client.get(profile1_path).status_code, status.HTTP_200_OK)
         response = self.user1_rest_client.put(
@@ -86,4 +99,152 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         new_profile1_path = reverse("v1:notification-profile:notificationprofile-detail", args=[new_profile1_pk])
         self.assertEqual(self.user1_rest_client.get(new_profile1_path).status_code, status.HTTP_200_OK)
 
-    # TODO: test more endpoints
+    def test_get_notification_profile_by_pk(self):
+        profile_pk = self.notification_profile1.pk
+        # /api/v1/notificationprofiles/<int:pk>/
+        response = self.user1_rest_client.get(
+            reverse("v1:notification-profile:notificationprofile-detail", args=[profile_pk])
+        )
+        response.render()
+        self.assertEqual(response.data["pk"], profile_pk)
+
+    def test_get_notification_profiles(self):
+        # /api/v1/notificationprofiles/
+        response = self.user1_rest_client.get(reverse("v1:notification-profile:notificationprofile-list"))
+        response.render()
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["pk"], self.notification_profile1.pk)
+
+    def test_post_new_notification_profile(self):
+        # /api/v1/notificationprofiles/
+        response = self.user1_rest_client.post(
+            reverse("v1:notification-profile:notificationprofile-list"),
+            {
+                "timeslot": self.timeslot2.pk,
+                "filters": [f.pk for f in self.notification_profile1.filters.all()],
+                "media": self.media_v1,
+                "phone_number": self.phone_number,
+                "active": self.notification_profile1.active,
+            },
+        )
+        response.render()
+        self.assertTrue(NotificationProfile.objects.filter(pk=response.data.get("pk")))
+
+    def test_delete_notification_profile(self):
+        profile_pk = self.notification_profile1.pk
+        # /api/v1/notificationprofiles/<int:pk>/
+        response = self.user1_rest_client.delete(
+            reverse("v1:notification-profile:notificationprofile-detail", args=[profile_pk])
+        )
+        response.render()
+        self.assertFalse(NotificationProfile.objects.filter(pk=profile_pk).first())
+
+    def test_get_timeslots(self):
+        # /api/v1/notificationprofiles/timeslots/
+        response = self.user1_rest_client.get(reverse("v1:notification-profile:timeslot-list"))
+        response.render()
+        default_timeslot = Timeslot.objects.filter(user=self.user1, name="All the time").first()
+        timeslot_pks = set([default_timeslot.pk, self.timeslot1.pk, self.timeslot2.pk])
+        response_pks = set([timeslot["pk"] for timeslot in response.data])
+        self.assertEqual(timeslot_pks, response_pks)
+
+    def test_create_new_timeslot(self):
+        # /api/v1/notificationprofiles/timeslots/
+        response = self.user1_rest_client.post(
+            reverse("v1:notification-profile:timeslot-list"),
+            {
+                "name": "test-timeslot",
+                "time_recurrences": [{"days": [1, 2, 3], "start": "10:00:00", "end": "20:00:00"}],
+            },
+        )
+        response.render()
+        self.assertTrue(Timeslot.objects.filter(user=self.user1, name="test-timeslot").first())
+
+    def test_get_timeslot_by_pk(self):
+        timeslot_pk = self.timeslot1.pk
+        # /api/v1/notificationprofiles/timeslots/<int:pk>/
+        response = self.user1_rest_client.get(reverse("v1:notification-profile:timeslot-detail", args=[timeslot_pk]))
+        response.render()
+        self.assertEqual(response.data["pk"], timeslot_pk)
+
+    def test_update_timeslot_name(self):
+        timeslot_pk = self.timeslot1.pk
+        new_name = "new-test-name"
+        # /api/v1/notificationprofiles/timeslots/<int:pk>/
+        response = self.user1_rest_client.put(
+            reverse("v1:notification-profile:timeslot-detail", args=[timeslot_pk]),
+            {"name": new_name, "time_recurrences": [{"days": [1, 2, 3], "start": "10:00:00", "end": "20:00:00"}]},
+        )
+        response.render()
+        self.assertEqual(Timeslot.objects.filter(pk=timeslot_pk).first().name, new_name)
+
+    def test_delete_timeslot(self):
+        timeslot_pk = self.timeslot1.pk
+        # /api/v1/notificationprofiles/timeslots/<int:pk>/
+        response = self.user1_rest_client.delete(reverse("v1:notification-profile:timeslot-detail", args=[timeslot_pk]))
+        response.render()
+        self.assertFalse(Timeslot.objects.filter(pk=timeslot_pk).first())
+
+    def test_get_filters(self):
+        # /api/v1/notificationprofiles/filters/
+        response = self.user1_rest_client.get(reverse("v1:notification-profile:filter-list"))
+        response.render()
+        self.assertTrue(len(response.data), 1)
+        self.assertEqual(response.data[0]["pk"], self.filter1.pk)
+
+    def test_create_new_filter(self):
+        # /api/v1/notificationprofiles/filters/
+        response = self.user1_rest_client.post(
+            reverse("v1:notification-profile:filter-list"),
+            {
+                "name": "test-filter",
+                "filter_string": f'{{"sourceSystemIds": [{self.source1.pk}], "tags": ["key1=value"]}}',
+            },
+        )
+        response.render()
+        self.assertTrue(Filter.objects.filter(user=self.user1, name="test-filter").first())
+
+    def test_get_filter_by_pk(self):
+        filter_pk = self.filter1.pk
+        # /api/v1/notificationprofiles/filters/<int:pk>/
+        response = self.user1_rest_client.get(reverse("v1:notification-profile:filter-detail", args=[filter_pk]))
+        response.render()
+        self.assertEqual(response.data["pk"], filter_pk)
+
+    def test_update_filter_name(self):
+        filter_pk = self.filter1.pk
+        new_name = "new-test-name"
+        # /api/v1/notificationprofiles/filters/<int:pk>/
+        response = self.user1_rest_client.put(
+            reverse("v1:notification-profile:filter-detail", args=[filter_pk]),
+            {
+                "name": new_name,
+                "filter_string": f'{{"sourceSystemIds": [{self.source1.pk}], "tags": ["key1=value"]}}',
+            },
+        )
+        response.render()
+        self.assertEqual(Filter.objects.filter(pk=filter_pk).first().name, new_name)
+
+    def test_delete_unused_filter_is_deleted(self):
+        filter_pk = self.filter2.pk
+        # /api/v1/notificationprofiles/filters/<int:pk>/
+        response = self.user1_rest_client.delete(reverse("v1:notification-profile:filter-detail", args=[filter_pk]))
+        response.render()
+        self.assertFalse(Filter.objects.filter(pk=filter_pk).first())
+
+    def test_delete_used_filter_is_not_deleted(self):
+        filter_pk = self.filter1.pk
+        # /api/v1/notificationprofiles/filters/<int:pk>/
+        response = self.user1_rest_client.delete(reverse("v1:notification-profile:filter-detail", args=[filter_pk]))
+        response.render()
+        self.assertTrue(Filter.objects.filter(pk=filter_pk).first())
+
+    def test_get_filterpreview(self):
+        # /api/v1/notificationprofiles/filterpreview/
+        response = self.user1_rest_client.post(
+            reverse("v1:notification-profile:filter-preview"),
+            {"sourceSystemIds": [self.source1.pk], "tags": [str(self.tag1)]},
+        )
+        response.render()
+        self.assertTrue(len(response.data), 1)
+        self.assertEqual(response.data[0]["pk"], self.incident1.pk)
